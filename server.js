50e0f5a7343e9d16c94591d1c512c9baa97bcc5b
LyovaMinasyan <lyova.minasyan.y@tumo.org>
2024-02-04 15:07:03 +0400

    Final work

--- server.js
+++ server.js
@@ -6,10 +6,13 @@ var app = express();
 app.set('view engine', 'ejs');
 
 const mongoose = require('mongoose');
+const { ObjectId } = require("mongoose").Types;
+// const { ObjectId } = require("mongodb");
 const connectionString = 'mongodb+srv://aram:admin1234@cluster0.lm98xcg.mongodb.net/Tumo';
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static('public'));
+app.use(express.static(path.join(_diranme, 'public')));
+
 
 
 app.get("/", function (req, res) {
@@ -43,9 +46,69 @@ app.post('/addFilm', (req, res) => {
                 name: name,
                 data: data
             })
-            res.json(result);
+            res.redirect('/');
+            // res.json(result);
         } catch (error) {
             console.error('Error retrieving movies:', error);
+        } finally {
+            mongoose.connection.close();
+        }
+    })
+});
+
+app.get('/delete/:id', function (req, res) {
+    var id = req.params.id;
+    mongoose.connect(connectionString, {useNewUrlParser: true, useUnifiedTopology: true});
+    const db = mongoose.connection;
+    db.on('error', console.error.bind(console, 'Connection error:'));
+    db.once('open', async () => {
+        try {
+            let result = await mongoose.connection.db.collection('films').deleteOne({_id: new ObjectId(id)});
+            res.redirect('/');
+        } catch (error) {
+            console.error('Error retrieving films:', error)
+        } finally {
+            mongoose.connection.close();
+        }
+    })
+})
+
+
+app.get("/update/:id", function (req, res) {
+
+    var id = req.params.id;
+    mongoose.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true });
+    const db = mongoose.connection;
+    db.on('error', console.error.bind(console, 'Connection error:'));
+    db.once('open', async () => {
+        try {
+            let result = await mongoose.connection.db.collection('films').findOne({_id: new ObjectId(id)})
+            res.render('../public/updateForm.ejs', {
+                obj: result
+            });
+            mongoose.connection.close();
+        } catch (error) {
+            console.error('Error retrieving film update page:', error);
+        } finally {
+            mongoose.connection.close();
+        }
+    })
+});
+
+app.post("/updateFilm", function (req, res) {
+
+    const {name, data, id} = req.body;
+
+
+    mongoose.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true });
+    const db = mongoose.connection;
+    db.on('error', console.error.bind(console, 'Connection error:'));
+    db.once('open', async () => {
+        try {
+            let result = await mongoose.connection.db.collection('films').updateOne({_id: new ObjectId(id)}, { $set: { name:name, data:data}})
+            res.redirect('/');
+        } catch (error) {
+            console.error('Error retrieving film update page:', error);
         } finally {
             mongoose.connection.close();
         }
@@ -55,4 +118,3 @@ app.post('/addFilm', (req, res) => {
 app.listen(3000, function () {
     console.log("Example is running on port 3000");
 });
-
